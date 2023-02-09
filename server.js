1f3d919271cd53244ef46bb329c7cb722a3b8e3e
ohjinhee <riricos153@naver.com>
2023-02-10 01:59:55 +0900

    node.js 기록

--- server.js
+++ server.js
@@ -4,6 +4,44 @@ const app = express();
 app.listen(8080, function() {    //  app.listen()은 원하는 포트에 서버를 오픈하는 문법이라고 보면 된다.
     console.log('listening on 8080')
 })
+// 여기까지가 서버를 띄우기 위해 작성할 기본 셋팅.
 
 // 위 코드를 잘 작성하고 New Terminal 눌러 터미널을 켜서 node server.js를 입력하면 서버가 뜬다.
 // 브라우저에서 localhost:8080 이라고 접속하면 확인가능하다. 
+
+
+
+// 서버는 요청을 처리하는 기계라고 말했다.
+// 브라우저를 켜서 주소창에 URL을 입력하시면 된다. 그게 가장쉬운 요청 방법이다.
+// 전문용어로 GET 요청이라고 한다.
+
+app.get('/pet', (요청, 응답) => { 
+    응답.send('펫용품 입니다.');
+});
+
+app.get('/beauty', (요청, 응답) => { 
+    응답.send('뷰티용품 입니다.');
+});
+// 그럼 누군가 우리 서버의 /pet 경로로 접속하면 '펫용품 입니다.' 라는 안내메세지를 띄워주는 서버가 만들어진다.  
+// 브라우저 켜서 localhost:8080/pet 이라고 접속하면 펫용품 입니다.라는 안내문이 뜬다.
+// (터미널에서 ctrl + c 누르시고 서버를 끈 다음 node server.js 입력해서 서버를 재시작 해야한다.)
+
+
+
+// 껐다 키기 귀찮으니 nodemon 설치를 해보자 
+// 터미널에서 npm install -g nodemon 입력하면 됩니다.
+// 이제 서버를 실행할때 nodemon server.js 라고 입력해주면 된다.
+// 그럼 파일 저장할때 마다 이제 지가 알아서 서버를 새로 시작해준다. 
+
+
+app.get('/', (요청, 응답) => { 
+    응답.sendFile(__dirname + '/index.html');
+});
+// 그럼 누군가 / 경로로 접속시 (/ 하나만 있으면 홈페이지 이다)
+// server.js랑 같은 경로에 있는 /index.html 이라는 파일을 보내준다. 
+// sendFile() 함수를 쓰면 파일을 보낼수 있다.
+// __dirname은 현재 파일의 경로를 뜻한다.
+
+app.get('/write', (요청, 응답) => { 
+    응답.sendFile(__dirname + '/write.html');
+});
