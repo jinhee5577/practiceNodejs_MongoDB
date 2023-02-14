41936140051353005f81f0ad6a1bc051222500b8
ohjinhee <riricos153@naver.com>
2023-02-15 03:15:35 +0900

    MongoDB 연동

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true})); 
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(8080, function() {    //  app.listen()은 원하는 포트에 서버를 오픈하는 문법이라고 보면 된다.
-    console.log('listening on 8080')
-})
-// 여기까지가 서버를 띄우기 위해 작성할 기본 셋팅.
+MongoClient.connect('mongodb+srv://admin:wlsgml153@jini.fpfne7k.mongodb.net/todoapp?retryWrites=true&w=majority', (에러, client) => { 
+    if (에러) return console.log(에러);
+    
+    // database접속이 완료되면 콜백함수를 실행해준다. 
+    app.listen(8080, function() {    //  app.listen()은 원하는 포트에 서버를 오픈하는 문법이라고 보면 된다.
+        console.log('listening on 8080');
+    });
+    // 여기까지가 서버를 띄우기 위해 작성할 기본 셋팅.
+});
+
 
 // 위 코드를 잘 작성하고 New Terminal 눌러 터미널을 켜서 node server.js를 입력하면 서버가 뜬다.
 // 브라우저에서 localhost:8080 이라고 접속하면 확인가능하다. 
