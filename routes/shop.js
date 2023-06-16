b7ba084f86bcba801b44e19f460855765e0af803
ohjinhee <riricos153@naver.com>
2023-06-17 01:23:32 +0900

    router파일 만들어서 server.js에 첨부

--- routes/shop.js
+++ routes/shop.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+// require('./파일경로') / require('라이브러리명')는
+// 다른 파일이나, 라이브러리를 여기에 첨부하겠다.
 
 // 연습용으로 URL route 2개를 만들었다.
 router.get('/shop/shirts', (요청, 응답) => {
@@ -13,3 +15,4 @@ router.get('/shop/pants', (요청, 응답) => {
 
 module.exports = router; // (내보낼 변수명)
 // 이파일에서 router라는 변수를 배출할게요. 라는 뜻이다.
+// 다른곳에서 shop.js를 가져다 쓸때 router라는 변수를 배출하겠다. 라는 뜻이다.
